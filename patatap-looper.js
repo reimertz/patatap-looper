20d9787db2961665f55ced071d65c74d89161356
Jacinto Shy <jacinto@thrillist.com>
2014-03-27 11:41:40 -0400

    Dynamic loops.

--- patatap-looper.js
+++ patatap-looper.js
@@ -1,34 +1,60 @@
 $(function(){
-  var beatPerLoop = 16,
-      BeatsPerMinute = 200,
+  var SL = {
+      beatsPerLoop: 16,
+      beatsPerMinute: 200,
+      _loops: {},
+      _stop: false,
+      add: function(name, beats) {
+        this._loops[name] = beats.split(' ');
+      },
+      remove: function(name, beats) {
+        delete this._loops[name];
+      },
+      list: function() {
+        var count = 0;
+        for (key in this._loops) {
+          console.log(key + ': ' + this._loops[key].join(' '));
+          count++;
+        }
+        console.log(count + ' loop(s)');
+      },
+      clear: function() {
+        this._loops = {};
+      },
+      stop: function() {
+        this._stop = true;
+      },
+      start: function() {
+        this._stop = false
+        superloop(0, this.beatsPerLoop, this.beatsPerMinute);
+      }
+  }
 
-      beats = {
-        base_loop     : 'e - - - e - - - e - - - e - - -',
-        snare_loop    : '- - o - - - o - - - o - - - o -',
-        love_loop     : '- p - h - e - a - r - t - m - -'
-      };
-  
-  function beatIt(loop, beat, bpl, bmp){
-    
+  function playBeat(loop, beat) {
     if(loop[beat] !== '-'){
       var e = jQuery.Event("keydown");
       e.which = loop[beat].toUpperCase().charCodeAt(0);
       $("input").val(String.fromCharCode(e.which));
       $("html").trigger(e);
     }
-
-    setTimeout(function(){
-      var nextBeat = (beat+1)%bpl; 
-      beatIt(loop, nextBeat, beatPerLoop, BeatsPerMinute);
-    }, (1000 * 60) / BeatsPerMinute);
-    
   }
 
-  function initBeats(beats, bpl, bmp){
-    _.each(beats, function(beat){
-      beatIt(beat.split(' '), 0, beatPerLoop, BeatsPerMinute);
+  function superloop(beat, bpl, bpm) {
+    _.each(SL._loops, function(loop) {
+      playBeat(loop, beat);
     });
+    if (SL._stop)
+      return
+    setTimeout(function(){
+      var nextBeat = (beat+1)%bpl;
+      superloop(nextBeat, bpl, bpm);
+    }, (1000 * 60) / bpm);
   }
-
-  initBeats(beats, beatPerLoop, BeatsPerMinute);
+  
+  superloop(0, SL.beatsPerLoop, SL.beatsPerMinute);
+  window.superloops = SL;
 });
+
+superloops.add('base', 'e - - - e - - - e - - - e - - -');
+superloops.add('snare', '- - o - - - o - - - o - - - o -');
+superloops.add('love', '- p - h - e - a - r - t - m - -');
