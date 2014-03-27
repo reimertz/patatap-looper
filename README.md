b64f1535c1c46597837c4383b34041814e58fb8b
Pierre Reimertz <pierre@pierrereimertz.com>
2014-03-27 01:06:54 -0700

    first commit, lets loop!

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 patatap-looper
 ==============
+
+Usage
+
+1. Copy paste the entire source code from patatap-looper.js
+2. go to http://www.patatap.com/
+3. Open the console and copy-paste the source code and press enter.
+
+If you want to create your own beats just look at the source code, 
+you'll get how it works..
+
+it supports beats per minute and beats per loop.
+
+PS. you can play at the same time as the looper is doing it's thang.. :)
+
+
+
---
+++ patatap-looper.js
@@ -0,0 +1,34 @@
+$(function(){
+  var beatPerLoop = 16,
+      BeatsPerMinute = 200,
+
+      beats = {
+        base_loop     : 'e - - - e - - - e - - - e - - -',
+        snare_loop    : '- - o - - - o - - - o - - - o -',
+        love_loop     : '- p - h - e - a - r - t - m - -'
+      };
+  
+  function beatIt(loop, beat, bpl, bmp){
+    
+    if(loop[beat] !== '-'){
+      var e = jQuery.Event("keydown");
+      e.which = loop[beat].toUpperCase().charCodeAt(0);
+      $("input").val(String.fromCharCode(e.which));
+      $("html").trigger(e);
+    }
+
+    setTimeout(function(){
+      var nextBeat = (beat+1)%bpl; 
+      beatIt(loop, nextBeat, beatPerLoop, BeatsPerMinute);
+    }, (1000 * 60) / BeatsPerMinute);
+    
+  }
+
+  function initBeats(beats, bpl, bmp){
+    _.each(beats, function(beat){
+      beatIt(beat.split(' '), 0, beatPerLoop, BeatsPerMinute);
+    });
+  }
+
+  initBeats(beats, beatPerLoop, BeatsPerMinute);
+});
