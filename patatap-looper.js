bbbeea7bdb62a587ca8221d52410efecd06e4bfd
Pierre Reimertz <pierre@pierrereimertz.com>
2014-03-27 09:06:54 -0700

    some code cleanup and optimizations

--- patatap-looper.js
+++ patatap-looper.js
@@ -1,34 +1,38 @@
 $(function(){
   var beatPerLoop = 16,
       BeatsPerMinute = 200,
-
-      beats = {
+      $objects = {
+        e: jQuery.Event("keydown"),
+        $input : $("input"),
+        $html : $("html")
+      };
+      
+      loops = {
         base_loop     : 'e - - - e - - - e - - - e - - -',
         snare_loop    : '- - o - - - o - - - o - - - o -',
         love_loop     : '- p - h - e - a - r - t - m - -'
       };
-  
-  function beatIt(loop, beat, bpl, bmp){
-    
+
+  function beatIt (loop, beat, bpl, bpm, $objects) {
+
     if(loop[beat] !== '-'){
-      var e = jQuery.Event("keydown");
-      e.which = loop[beat].toUpperCase().charCodeAt(0);
-      $("input").val(String.fromCharCode(e.which));
-      $("html").trigger(e);
+      $objects.e.which = loop[beat].toUpperCase().charCodeAt(0);
+      $objects.$input.val(String.fromCharCode($objects.e.which));
+      $objects.$html.trigger($objects.e);
     }
 
     setTimeout(function(){
-      var nextBeat = (beat+1)%bpl; 
-      beatIt(loop, nextBeat, beatPerLoop, BeatsPerMinute);
+
+      var nextBeat = ((beat+1) === bpl) ?  0 : (beat+1); 
+      beatIt(loop, nextBeat, bpl, bpm, $objects);
     }, (1000 * 60) / BeatsPerMinute);
-    
   }
 
-  function initBeats(beats, bpl, bmp){
-    _.each(beats, function(beat){
-      beatIt(beat.split(' '), 0, beatPerLoop, BeatsPerMinute);
+  function initBeats (loops, bpl, bpm, $objects) {
+    _.each(loops, function(loop){
+      beatIt(loop.split(' '), 0, bpl, bpm, $objects);
     });
   }
 
-  initBeats(beats, beatPerLoop, BeatsPerMinute);
+  initBeats(loops, beatPerLoop, BeatsPerMinute, $objects);
 });
